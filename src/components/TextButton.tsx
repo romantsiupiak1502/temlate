98ab9b1f0aadd8032c453811752b44b9671ab083
romantsiupiak1502 <roman.ts@nerdysoft.com>
2021-02-18 15:32:15 +0200

    Modified text button

--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { styled } from '../styles';
+import { ButtonText } from './Text';
 
 interface ITextButtonStyledProps {
   isDisabled?: boolean
 }
 
-const TButtonWrapper = styled.div<ITextButtonStyledProps>`
+const TextButtonWrapper = styled.div<ITextButtonStyledProps>`
   width: max-content;
   height: max-content;
   padding-top: 8px;
@@ -15,7 +16,7 @@ const TButtonWrapper = styled.div<ITextButtonStyledProps>`
   color: ${ ({ theme, isDisabled }) => isDisabled ? theme.colors.gray : theme.colors.black };
 `;
 
-interface ITextButtonProps extends React.ComponentPropsWithoutRef<'div'> {
+interface ITextButtonProps {
   text: string;
   isDisabled?: boolean;
   onClick: () => void;
@@ -24,8 +25,10 @@ interface ITextButtonProps extends React.ComponentPropsWithoutRef<'div'> {
 export const TextButton: React.FC<ITextButtonProps> = props => {
   const { text, onClick, isDisabled } = props;
     return (
-        <TButtonWrapper onClick={onClick} isDisabled={isDisabled}>
-          {text}
-        </TButtonWrapper>
+        <TextButtonWrapper onClick={onClick} isDisabled={isDisabled}>
+          <ButtonText>
+            {text}
+          </ButtonText>
+        </TextButtonWrapper>
     );
 };
