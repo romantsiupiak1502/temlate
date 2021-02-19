6dced9ffd55365c9be06f5c488eeea97f6abd8b3
romantsiupiak1502 <roman.ts@nerdysoft.com>
2021-02-19 16:04:55 +0200

    Remove div and add flex

--- src/containers/AboutUs/AboutUs.tsx
+++ src/containers/AboutUs/AboutUs.tsx
@@ -27,6 +27,8 @@ const ContentWrapper = styled.div`
 const AboutUsWrapper = styled.div`
   width: 600px;
   height: 250px;
+  display: flex;
+  flex-direction: column;
   background-color: ${ ({theme}) => theme.colors.primary };
   opacity: 0.65;
   padding: 20px;
@@ -40,11 +42,9 @@ export const AboutUs: React.FC = () => {
       <ContentWrapper>
         <AboutUsWrapper>
           <H1>About us</H1>
-          <div>
-            <H3>Hello, we are a group of young developers who are happy to welcome you to our social network.
-              If you have not yet registered, do not hesitate and try our product soon, and if you have
-              tried, enjoy it.</H3>
-          </div>
+          <H3>Hello, we are a group of young developers who are happy to welcome you to our social network.
+            If you have not yet registered, do not hesitate and try our product soon, and if you have
+            tried, enjoy it.</H3>
         </AboutUsWrapper>
         <img src={AboutUsTeamImage} />
       </ContentWrapper>
