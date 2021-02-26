62650faa18c2a9ca6f7b3532debc92ea70dfc929
romantsiupiak1502 <roman.ts@nerdysoft.com>
2021-02-26 17:59:50 +0200

    Add storage service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,10 @@
+import { localStorageService } from './storageService';
 import { apiServiceCreator, mockApiServiceCreator } from "./apiService";
 
 export const apiService = apiServiceCreator("");
 
 export const mockApiService = mockApiServiceCreator();
+
+export const storageService = localStorageService();
 
 export * as languageService from './languageService';
---
+++ src/services/storageService.ts
@@ -0,0 +1,16 @@
+type ISetItem = (key: string, value: any) => void;
+type IGetItem = <T>(key: string) => T | null;
+type IRemoveItem = (key: string) => void;
+
+export const localStorageService = () => {
+  const setItem: ISetItem = (key: string, value: any) => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  const getItem: IGetItem = (key) => {
+    const value = window.localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  };
+  const removeItem: IRemoveItem = (key: string) => {
+    window.localStorage.removeItem(key);
+  };
+};
