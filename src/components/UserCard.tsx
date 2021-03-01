badd65bad6f709afe5067867c48d7f6b98948a20
romantsiupiak1502 <roman.ts@nerdysoft.com>
2021-03-01 20:43:27 +0200

    Add user-card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,40 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+import { UserAvatarConst } from '../consts';
 import { styled } from '../styles';
 
-import { H3 } from './Text';
-import { IconButton } from './IconButton';
-import { SendMessageIcon } from './icons';
+import { UserAvatar } from './UserAvatar';
+import { H1, H4 } from './Text';
 
 const UserCardContainer = styled.div`
-  width: 230px;
-  height: 300px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
+  width: 550px;
+  height: 210px;
   border: 1px solid;
   border-radius: 8px;
+  padding: 16px;
+  display: flex;
+  background-color: ${ ({ theme }) => theme.colors.backgroundGray };
+  cursor: pointer;
+  &:hover {
+    background-color: ${ ({ theme }) => theme.colors.white };
+  }
 `;
 
 const AvatarContainer = styled.div`
-  width: 100%;
-  height: 70%;
-  border-top-left-radius: 8px;
+  width: 40%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border-top-right-radius: 8px;
-  background-color: ${ ({ theme }) => theme.colors.primary };
-  opacity: 0.7;
 `;
 
-const SendButtonContainer = styled.div`
+const UserInformContainer = styled.div`
+  width: 60%;
+  height: 100%;
+`;
+
+const UserNameContainer = styled.div`
   width: 100%;
-  height: 20%;
-  color: ${ ({ theme }) => theme.colors.white };
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: ${ ({ theme }) => theme.colors.primary };
+  border-bottom: 1px solid;
+`;
+
+const TextStyled = styled(H4)`
+  color: ${ ({ theme }) => theme.colors.gray };
+`;
+
+const UserInfo = styled.div`
+  width: 100%;
+  height: 70%;
+  display: flex;
+  align-items: center;
 `;
 
 interface IUserCardProps {
@@ -42,23 +61,41 @@ interface IUserCardProps {
   avatar: React.ReactElement;
   userName: string;
   email: string;
+  city: string;
+  street: string;
+  phone: string;
+  webSite: string;
 };
 
 export const UserCard: React.FC<IUserCardProps> = props => {
-  const { id, avatar, userName, email } = props;
+
+  const { id, avatar, userName, email, city, phone, street } = props;
+
   return (
     <UserCardContainer>
       <AvatarContainer>
-        <SendButtonContainer>
-          <Link to=''>
-            <IconButton text='' icon={ <SendMessageIcon size='30px'/> } onClick={ () => {
-            } }/>
-          </Link>
-        </SendButtonContainer>
-        { avatar }
+        <UserAvatar text='RT' size={ UserAvatarConst.BIG_AVATAR } onClick={ () => {
+        } }/>
       </AvatarContainer>
-      <H3>{ userName }</H3>
-      <H3>{ email }</H3>
+      <UserInformContainer>
+        <UserNameContainer>
+          <H1>{ userName }</H1>
+          <TextStyled>{ city }</TextStyled>
+        </UserNameContainer>
+        <UserInfo>
+          <ul>
+            <li>
+              <TextStyled>email: { email }</TextStyled>
+            </li>
+            <li>
+              <TextStyled>street: { street }</TextStyled>
+            </li>
+            <li>
+              <TextStyled>phone: { phone }</TextStyled>
+            </li>
+          </ul>
+        </UserInfo>
+      </UserInformContainer>
     </UserCardContainer>
   );
 };
