184c88f58e16e5f80a943f312f8c5df09200938b
romantsiupiak1502 <roman.ts@nerdysoft.com>
2021-03-19 16:54:25 +0200

    Fix sidebar for mobile responsive

--- src/containers/Root/Sidebar.tsx
+++ src/containers/Root/Sidebar.tsx
@@ -21,16 +21,24 @@ const getDropdownItems: IGetDropdownItems = (onSortByName, onSortByEmail, t) => 
   ];
 }
 
-const Wrapper = styled.div`
+interface IOpenSidebarProps {
+  isOpen: boolean;
+}
+
+const Wrapper = styled.div<IOpenSidebarProps>`
   background-color: ${ ({ theme }) => theme.colors.backgroundGray };
   border-left: 2px solid ${ ({ theme }) => theme.colors.gray };
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 16px;
+  position: ${ ({ isOpen }) => isOpen && 'absolute' };
 
-  @media ${device.mobileXL} {
-    display: none;
+  @media ${ device.mobileXL } {
+    top: 104px;
+    right: 0px;
+    display: ${ ({ isOpen }) => !isOpen && 'none' };
+    }
   }
 `;
 
@@ -40,6 +48,42 @@ const InputStyled = styled(Input)`
 
 const SortButton = styled.div`
   margin-top: 32px;
+`;
+
+const BurgerMenu = styled.div`
+  display: none;
+  @media ${device.mobileXL} {
+    display: block;
+    transform-origin: 16px 11px;
+    float: left;
+    outline: 0;
+    border: 0;
+    padding: 12px;
+
+    &.active {
+      span:nth-of-type(1) {
+        transform: rotate(45deg) translate(2px, 2px);
+        width: 24px;
+      }
+
+      span:nth-of-type(2) {
+        opacity: 0;
+        pointer-events: none;
+      }
+
+      span:nth-of-type(3) {
+        transform: rotate(-45deg) translate(4px, -4px);
+        width: 24px;
+      }
+  }
+`;
+
+const Bar = styled.span`
+  display: block;
+  width: 24px;
+  height: 1px;
+  margin-bottom: 3px;
+  background-color: ${({ theme }) => theme.colors.primary };
 `;
 
 interface ISidebarProps {
@@ -52,24 +96,41 @@ export const Sidebar: React.FC<ISidebarProps> = props => {
   const { onSearchByNameChange, onSearchByEmailChange, onAddUserClick } = props;
   const { t } = useTranslation();
 
+  const [isOpenMenu, setIsOpenMenu] = React.useState(false);
+
+  const toggleOpenMenu = () => {
+    setIsOpenMenu(prevState => !prevState);
+  }
+
   return (
-    <Wrapper>
-      <IconTextButton
-        text={t("ADD_USER")}
-        icon={ <PlusIcon size='25px'/> }
-        onClick={ onAddUserClick }
-      />
-      <InputStyled placeholder={t("SEARCH_BY_NAME")} onChange={ onSearchByNameChange }/>
-      <InputStyled placeholder={t("SEARCH_BY_EMAIL")} onChange={ onSearchByEmailChange }/>
-      <SortButton>
-        <Dropdown
-          items={ getDropdownItems(() => {}, () => {}, t) }
-          Component={ <IconTextButton
-            text={t("SORT")}
-            icon={ <DownArrowIcon size='15px'/> }
-          /> }
+    <>
+      <BurgerMenu onClick={ toggleOpenMenu } className={isOpenMenu ? 'active': ''}>
+        <Bar />
+        <Bar />
+        <Bar />
+      </BurgerMenu>
+      <Wrapper isOpen={ isOpenMenu }>
+        <IconTextButton
+          text={ t("ADD_USER") }
+          icon={ <PlusIcon size='25px'/> }
+          onClick={ onAddUserClick }
         />
-      </SortButton>
-    </Wrapper>
+        <InputStyled placeholder={ t("SEARCH_BY_NAME") } onChange={ onSearchByNameChange }/>
+        <InputStyled placeholder={ t("SEARCH_BY_EMAIL") } onChange={ onSearchByEmailChange }/>
+        <SortButton>
+          <Dropdown
+            items={ getDropdownItems(() => {
+              setIsOpenMenu(false)
+            }, () => {
+              setIsOpenMenu(false)
+            }, t) }
+            Component={ <IconTextButton
+              text={ t("SORT") }
+              icon={ <DownArrowIcon size='15px'/> }
+            /> }
+          />
+        </SortButton>
+      </Wrapper>
+    </>
   );
 };
